54e2c5866d318e3e6f8f3aa7ec0fb87b913d7cac
Ken Chu <chu.kenneth.y@gmail.com>
2015-02-08 23:49:32 -0500

    Revert "More efficient problem 9 solution"

--- 9/9.hs
+++ 9/9.hs
@@ -1,12 +1,1 @@
-{-|
- - A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
- -
- - a2 + b2 = c2
- - For example, 32 + 42 = 9 + 16 = 25 = 52.
- -
- - There exists exactly one Pythagorean triplet for which a + b + c = 1000.
- - Find the product abc.
- - -}
-
- tripletProduct n = head [a*b*c | a <- [1..n-2], b <- [a..(n-a-1)], c <- [b..(n-a-b)], a+b+c==n, a^2 + b^2 == c^2]
- ---tripletProduct 1000
+--take 1 [a*b*c | a <- [1..1000], b <- [1..1000], c <- [1..1000], a<b, b<c, a+b+c==1000, (a*a)+(b*b)==(c*c)]
